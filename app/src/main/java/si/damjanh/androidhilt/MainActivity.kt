204e933b018569c85a574cc896f7d5d4ffdc065c
damjanh <damjan.hudobreznik@gmail.com>
2020-06-19 09:26:50 +0200

    Implement recycler view adapter.

--- app/src/main/java/si/damjanh/androidhilt/MainActivity.kt
+++ app/src/main/java/si/damjanh/androidhilt/MainActivity.kt
@@ -2,11 +2,17 @@ package si.damjanh.androidhilt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = WordListAdapter(this)
+        recyclerview.adapter = adapter
+        recyclerview.layoutManager = LinearLayoutManager(this)
     }
 }
