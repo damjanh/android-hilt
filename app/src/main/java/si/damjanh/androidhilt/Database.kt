0d9caa862c30d47e2b6f8e25796065b91c50fd5a
damjanh <damjan.hudobreznik@gmail.com>
2020-06-19 09:07:34 +0200

    Implement Room database.

---
+++ app/src/main/java/si/damjanh/androidhilt/Database.kt
@@ -0,0 +1,33 @@
+package si.damjanh.androidhilt
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Word::class], version = 1, exportSchema = false)
+public abstract class WordDatabase : RoomDatabase() {
+
+    abstract fun wordDao(): WordDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: WordDatabase? = null
+
+        fun getDatabase(context: Context): WordDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instante = Room.databaseBuilder(
+                    context.applicationContext,
+                    WordDatabase::class.java,
+                    "word_database"
+                ).build()
+                INSTANCE = instante
+                return instante
+            }
+        }
+    }
+}
--- app/src/main/java/si/damjanh/androidhilt/WordDao.kt
+++ app/src/main/java/si/damjanh/androidhilt/WordDao.kt
@@ -1,5 +1,6 @@
 package si.damjanh.androidhilt
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
@@ -8,7 +9,7 @@ import androidx.room.Query
 @Dao
 interface WordDao {
     @Query("SELECT * from word_table ORDER BY word ASC")
-    fun getWords(): List<Word>
+    fun getWords(): LiveData<List<Word>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(word: Word)
