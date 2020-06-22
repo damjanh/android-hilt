a98e00357adf5b2508b928c99239eda777d41104
damjanh <damjan.hudobreznik@gmail.com>
2020-06-22 12:16:37 +0200

    Implement image select and permission handling.

--- app/src/main/java/si/damjanh/androidhilt/ui/blur/BlurFragment.kt
+++ app/src/main/java/si/damjanh/androidhilt/ui/blur/BlurFragment.kt
@@ -1,15 +1,39 @@
 package si.damjanh.androidhilt.ui.blur
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_blur.*
 import si.damjanh.androidhilt.R
+import java.util.*
 
 @AndroidEntryPoint
 class BlurFragment : Fragment() {
+
+    private val REQUEST_CODE_IMAGE = 100
+    private val REQUEST_CODE_PERMISSIONS = 101
+
+    private val KEY_PERMISSIONS_REQUEST_COUNT = "KEY_PERMISSIONS_REQUEST_COUNT"
+    private val MAX_NUMBER_REQUEST_PERMISSIONS = 2
+    private val permissions = listOf(
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
+
+    private var permissionRequestCount: Int = 0
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,5 +45,99 @@ class BlurFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        savedInstanceState?.let {
+            permissionRequestCount = it.getInt(KEY_PERMISSIONS_REQUEST_COUNT, 0)
+        }
+
+        button_select_image.setOnClickListener {
+            if (!requestPermissionsIfNecessary()) {
+                return@setOnClickListener
+            }
+
+            val chooseIntent = Intent(
+                Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            )
+            startActivityForResult(chooseIntent, REQUEST_CODE_IMAGE)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(KEY_PERMISSIONS_REQUEST_COUNT, permissionRequestCount)
+    }
+
+    private fun requestPermissionsIfNecessary(): Boolean {
+        if (!checkAllPermissions()) {
+            if (permissionRequestCount < MAX_NUMBER_REQUEST_PERMISSIONS) {
+                permissionRequestCount += 1
+                ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    permissions.toTypedArray(),
+                    REQUEST_CODE_PERMISSIONS
+                )
+                return false
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    R.string.set_permissions_in_settings,
+                    Toast.LENGTH_LONG
+                ).show()
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun checkAllPermissions(): Boolean {
+        var hasPermissions = true
+        for (permission in permissions) {
+            hasPermissions = hasPermissions and isPermissionGranted(permission)
+        }
+        return hasPermissions
+    }
+
+    private fun isPermissionGranted(permission: String) =
+        ContextCompat.checkSelfPermission(requireContext(), permission) ==
+                PackageManager.PERMISSION_GRANTED
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            requestPermissionsIfNecessary()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                REQUEST_CODE_IMAGE -> data?.let { handleImageRequestResult(data) }
+                else -> Log.d("BlurFragment","Unknown request code.")
+            }
+        } else {
+            Log.e("BlurFragment", String.format("Unexpected Result code %s", resultCode))
+        }
+    }
+
+    private fun handleImageRequestResult(intent: Intent) {
+        val imageUri: Uri? = intent.clipData?.getItemAt(0)?.uri ?: intent.data
+
+        if (imageUri == null) {
+            Log.e("BlurFragment", "Invalid input image Uri.")
+            return
+        }
+
+        image_view.setImageURI(imageUri)
+
+       // TODO: Show selected image and enable blur controls.
+
     }
 }
